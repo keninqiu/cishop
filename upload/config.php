02ee0f9076372f0a38f9e2d4a2cfc3e6308f0053
Ken Qiu <kqiu@drmg.ca>
2017-12-17 21:18:21 -0500

    first commit

---
+++ upload/config.php
@@ -0,0 +1,30 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://cishop/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://cishop/');
+
+// DIR
+define('DIR_APPLICATION', '/Library/WebServer/Documents/cishop/upload/catalog/');
+define('DIR_SYSTEM', '/Library/WebServer/Documents/cishop/upload/system/');
+define('DIR_IMAGE', '/Library/WebServer/Documents/cishop/upload/image/');
+define('DIR_STORAGE', '/Library/WebServer/Documents/cishop/storage/');
+define('DIR_LANGUAGE', DIR_APPLICATION . 'language/');
+define('DIR_TEMPLATE', DIR_APPLICATION . 'view/theme/');
+define('DIR_CONFIG', DIR_SYSTEM . 'config/');
+define('DIR_CACHE', DIR_STORAGE . 'cache/');
+define('DIR_DOWNLOAD', DIR_STORAGE . 'download/');
+define('DIR_LOGS', DIR_STORAGE . 'logs/');
+define('DIR_MODIFICATION', DIR_STORAGE . 'modification/');
+define('DIR_SESSION', DIR_STORAGE . 'session/');
+define('DIR_UPLOAD', DIR_STORAGE . 'upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'mysql');
+define('DB_DATABASE', 'cishop');
+define('DB_PORT', '3306');
+define('DB_PREFIX', '');
